0c7a5929543646d23efd6ab6dec08a7cb5f59163
Candice Han <jeeyoung.han82@gmail.com>
2020-08-19 17:57:57 +0200

    CREATE 'createNewBlock 'feature

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,72 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+import * as CryptoJS from "crypto-js";
+
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const komdori = new Human("Camille", 27, "male");
+const genesisBlock: Block = new Block(
+  0,
+  "202020202020202020",
+  "",
+  "hello",
+  1234567
+);
 
-const candice = {
-  name: "Jeeyoung",
-  gender: "female",
-  age: 27,
+let blockchain: Block[] = [genesisBlock];
+
+// console.log(blockchain);
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimestamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+  return newBlock;
 };
 
-const sayHi = (anyone: Human): string => {
-  return `Hello ${anyone.name}. You are ${anyone.age}, and you are a ${anyone.gender}!`;
-};
-
-console.log(sayHi(komdori));
-
+console.log(createNewBlock("hello"), createNewBlock("bye bye"));
 export {};
